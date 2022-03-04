5e6c10483746aeb7709dbcb15cc9e43c369660dd
An <ancmsvn@gmail.com>
2022-03-05 02:05:34 +0700

    add example

---
+++ example/src/index.ts
@@ -0,0 +1,11 @@
+// import hello from 'https://github.com'
+//
+// console.log(123, hello)
+
+// import('http://localhost:3005/index.js').then((mdl) => {
+//   console.log(mdl)
+// })
+
+import('http:///$HOST/index.js').then((mdl) => {
+  console.log(mdl)
+})
